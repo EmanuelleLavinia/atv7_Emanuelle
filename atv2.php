59ef2d144bda042473c70ba167f869246c5ea5c6
EmanuelleLavinia <0001104489@senaimgaluno.com.br>
2025-05-30 14:47:01 -0300

    Update atv2.php

--- atv2.php
+++ atv2.php
@@ -17,56 +17,51 @@ class funcionario  {
     }
 }
 
-class gerente extends funcionarios {
-
-    private $quantidadeDePortas;
-
-    public function __construct($marca, $modelo, $ano, $cor, $quantidadeDePortas){
-        $this->quantidadeDePortas = $quantidadeDePortas;
-        parent::__construct($marca, $modelo, $ano, $cor);
-    }
-
-    public function getQuantidadeDePortas(){
-        return $this->quantidadeDePortas;
-    }
-}
-
-
-
-
-
-
-    public function calcularSalario(){
-    return "Salário Base: {$this->salarioBase}";
-    }
-    public function exibirDados(){
-    return "Nome: {$this->nome}, CPF: {$this->cpf}";
-}
-
 class gerente extends funcionario {
-    public percentualDeBonus ();
-    public function calcularSalario($valor);
-     $this->salarioBase += $percentualDeBonus;
+
+    private $percentualDeBonus;
+
+    public function __construct($nome, $cpf, $salarioBase, $percentualDeBonus){
+        $this->percentualDeBonus =  $percentualDeBonus;
+        parent::__construct($nome, $cpf, $salarioBase);
+    }
+
+    public function getPercentualDeBonus(){
+        return $this->percentualDeBonus;
+    }
 }
 
-class estagiario {
-    public percentualDeBonus ();
-    public function calcularSalario($valor);
-     $this->salarioBase=+ 
-    
+class estagiario extends funcionario {
+
+    private $horasEstagio;
+    private $valorHora;
+
+   public function __construct($nome, $cpf, $salarioBase, $horasEstagio, $valorHora){
+        $this->horasEstagio = $horasEstagio;
+        $this->valorHora = $valorHora;
+        parent::__construct($nome, $cpf, $salarioBase);
+    }
+
+    public function getHorasEstagio(){
+        return $this->horasEstagio;
+    }
+     public function getValorHora(){
+        return $this->valorHora;
+    }
 }
 
-class funcionarioComum {
-    public function calcularSalario();
-    public percentualDeBonus ();
-}
 
-class dados  {
-    public $nome;
-    public $tipo;
-    public $salarioCalculado;
-}
 
-echo exibirInformacoes() . "<br>";
-}
+$f1 = new gerente('Thiago', '000.000.000-00', 10000, 20);
+echo $f1->exibirDados();
+echo ", Percentual de bonus: <br>" . $f1->getPercentualDeBonus();
+
+$f2 = new estagiario('Mateus', '111.111.111-11', 2000, 2, 50);
+echo $f2->exibirDados();
+echo ", Horas de estagio: " . $f2->getHorasEstagio();
+echo ", Valor por hora: " . $f2->getValorHora();
+
+$fc = new funcionario('<br>Martins', '222.222.222-22', 5000);
+echo $fc->exibirDados();
+
 ?>
